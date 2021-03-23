65f04b70567baa4d07f3f0cc380063204fa2b253
namchays <bautienhoa@gmail.com>
2021-03-23 09:25:16 +0700

    sua loi dong ho, sua loi tim kiem de thi

--- src/main/java/fpt/tracnghiem/controller/ThiController.java
+++ src/main/java/fpt/tracnghiem/controller/ThiController.java
@@ -1,16 +1,21 @@
 package fpt.tracnghiem.controller;
 
+import java.sql.Time;
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,10 +28,14 @@ import fpt.tracnghiem.entity.CauHoi;
 import fpt.tracnghiem.entity.DeThi;
 import fpt.tracnghiem.entity.Lop;
 import fpt.tracnghiem.entity.MonHoc;
+import fpt.tracnghiem.entity.Role;
+import fpt.tracnghiem.entity.TaiKhoan;
 import fpt.tracnghiem.service.CauHoiService;
 import fpt.tracnghiem.service.DeThiService;
 import fpt.tracnghiem.service.LopService;
 import fpt.tracnghiem.service.MonHocService;
+import fpt.tracnghiem.service.RoleService;
+import fpt.tracnghiem.service.TaiKhoanService;
 
 @Controller
 @RequestMapping(value = "/user")
@@ -35,8 +44,10 @@ public class ThiController {
 	private CauHoiService cauHoiService;
 	@Autowired
 	private DeThiService dethiService;
-	
-
+	@Autowired
+	private TaiKhoanService taiKhoanService;
+	@Autowired
+	private RoleService roleService;
 	@Autowired
 	private LopService lopService;
 	@Autowired
@@ -44,7 +55,8 @@ public class ThiController {
 	
 	
 	@RequestMapping(value = "/thi/page/{page}",method = RequestMethod.GET)
-	public String userInfo(HttpServletRequest req, Model model,@PathVariable(name = "page",required = false) Integer page) {
+	public ModelAndView userInfo(HttpServletRequest req,@PathVariable(name = "page",required = false) Integer page) {
+		ModelAndView mav = new ModelAndView();
 		if(page == null || page < 1) {
 			page=1;
 		}
@@ -57,13 +69,13 @@ public class ThiController {
 			pageDethi = dethiService.findPaginated(page-1, MyConstances.HOMEPAGE_SIZE);
 			dsDethi = pageDethi.getContent();
 		}
-		model.addAttribute("currentPage", page);
-		model.addAttribute("totalPages", pageDethi.getTotalPages());
-		model.addAttribute("totalItems", pageDethi.getTotalElements());
-		model.addAttribute("dsDeThi",dsDethi);
-		model.addAttribute("listLop", listLop);
-		model.addAttribute("listMonHoc", listMonhoc);
-		return "user/thi/listExam";
+	
+		mav.addObject("currentPage", page);
+		mav.addObject("totalPages", pageDethi.getTotalPages());
+		mav.addObject("totalItems", pageDethi.getTotalElements());
+		mav.addObject("dsDeThi",dsDethi);
+		ShowFormFormListExam(mav);
+		return mav;
 	}
 	
 	@PostMapping(value = "/thi/page/{page}")
@@ -75,15 +87,39 @@ public class ThiController {
 		}else {
 			 dsDethi =(List<DeThi>) dethiService.findByTenDeContaining(keyword);
 		}
-	
+		ShowFormFormListExam(mav);
 		mav.addObject("dsDeThi",dsDethi);
-		mav.setViewName("user/thi/listExam");
 		return mav;
 	}
 	
+	private void ShowFormFormListExam(ModelAndView mav) {
+		Optional<Role> role = roleService.findByRoleName(MyConstances.ROLE_USER);
+		List<TaiKhoan> Top6TaiKhoan = taiKhoanService.findTop6UserMaxPoint(role.get());
+		mav.addObject("TopUser",Top6TaiKhoan);
+		List<MonHoc> listMonhoc = (List<MonHoc>) monHocService.getAllMonHoc();
+		List<Lop> listLop = (List<Lop>) lopService.getAllLop();
+		mav.addObject("listLop", listLop);
+		mav.addObject("listMonHoc", listMonhoc);
+		mav.setViewName("/user/thi/listExam");
+	}
 	
 	@RequestMapping(value="/thi/{idDe}")
-	ModelAndView StartExam(@PathVariable int idDe) {
+	ModelAndView GetDataToThamGiaThi(HttpServletRequest req,@PathVariable int idDe) {
+		ModelAndView mav = new ModelAndView();
+		List<CauHoi> listCauHoi = cauHoiService.findAllByIdDeThi(idDe);
+		Optional<DeThi> deThi = dethiService.findById(idDe);
+		int ThoiGianThi = deThi.get().getThoiGianThi();
+		Date date = new Date();
+		Timestamp NgayGioKetThuc = new  Timestamp(date.getTime()+ ThoiGianThi *60000);
+		HttpSession session = req.getSession();
+		session.setAttribute("NgayGioKetThuc", NgayGioKetThuc);
+		mav.addObject("listCauHoi", listCauHoi);
+		mav.setViewName("redirect:/user/ThamGiaThi/"+idDe);
+		return mav;
+	}
+	
+	@RequestMapping(value="/ThamGiaThi/{idDe}")
+	ModelAndView StartExam(HttpServletRequest req,@PathVariable int idDe) {
 		ModelAndView mav = new ModelAndView();
 		List<CauHoi> listCauHoi = cauHoiService.findAllByIdDeThi(idDe);
 		Optional<DeThi> deThi = dethiService.findById(idDe);
@@ -91,14 +127,44 @@ public class ThiController {
 			mav.addObject("deThi", deThi.get());
 			mav.addObject("thoiGian", deThi.get().getThoiGianThi());
 		}
-		
-		
+		HttpSession session = req.getSession();
+		Timestamp TimeEnd =(Timestamp) session.getAttribute("NgayGioKetThuc");
+		mav.addObject("TimeEnd", TimeEnd);
 		mav.addObject("listCauHoi", listCauHoi);
 		mav.setViewName("/user/thi/startExam");
 		return mav;
 	}
 	
-
+	@RequestMapping(value = "/findByMonHoc/{idMonHoc}")
+	ModelAndView findByMonHoc(HttpServletRequest req,@PathVariable int idMonHoc,@Param("keyword") String keyword) {
+		ModelAndView mav = new ModelAndView();
+		List<DeThi> dsDethi=null;
+		Optional<MonHoc> monHoc = monHocService.FindById(idMonHoc);
+		if(monHoc.isPresent()) {
+			dsDethi =(List<DeThi>) dethiService.findByMonHoc(monHoc.get());
+		}
+		if(keyword!=null) 
+			dsDethi =dethiService.filterByKeyword(keyword, dsDethi);
+		mav.addObject("dsDeThi",dsDethi);
+		ShowFormFormListExam(mav);
+		return mav;
+		
+	}
+	@RequestMapping(value = "/findByLop/{idLopHoc}")
+	ModelAndView findByLopHoc(HttpServletRequest req,@PathVariable int idLopHoc,@Param("keyword") String keyword) {
+		ModelAndView mav = new ModelAndView();
+		List<DeThi> dsDethi=null;
+		Optional<Lop> lop = lopService.findByID(idLopHoc);
+		if(lop.isPresent()) {
+			dsDethi =(List<DeThi>) dethiService.findByLop(lop.get());
+		}
+		if(keyword!=null) 
+			dsDethi =dethiService.filterByKeyword(keyword, dsDethi);
+		mav.addObject("dsDeThi",dsDethi);
+		ShowFormFormListExam(mav);
+		return mav;
+		
+	}
 	
 
 	
--- src/main/java/fpt/tracnghiem/repository/TaiKhoanRepository.java
+++ src/main/java/fpt/tracnghiem/repository/TaiKhoanRepository.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import fpt.tracnghiem.entity.Role;
 import fpt.tracnghiem.entity.TaiKhoan;
 
 @Repository
 public interface TaiKhoanRepository extends CrudRepository<TaiKhoan, String> {
 	public List<TaiKhoan> findByUsernameAndPassword(String username,String password);
+	public List<TaiKhoan> findTop6ByRoleOrderByDiemTichLuyDesc(Role role);
 }
